50f304cdc9d542fdeae486406efe3cef9570a440
1224jatin <jatinsaini54083@gmail.com>
2026-03-10 16:09:50 +0530

    10 -march 2026 (pending shoping sceen) 1

--- lib/officeTaskScreen.dart
+++ lib/officeTaskScreen.dart
@@ -1,12 +1,21 @@
+import 'package:app_test1/taskCardScreen.dart';
 import 'package:app_test1/taskCategories.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Officetaskscreen extends StatefulWidget{
+
   @override
   State<StatefulWidget> createState() => _Officetaskscreen();
 }
 class _Officetaskscreen extends State<Officetaskscreen>{
+  List<Map<String,dynamic>>officeTaskList =
+  [
+    {"task" : "Assignment", "iscompleted": false},
+    {"task" : "meeting" , "iscompleted": false },
+    {"task" : "Task" , "iscompleted": false },
+    {"task" : "Project" , "iscompleted": false }
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,33 +28,44 @@ class _Officetaskscreen extends State<Officetaskscreen>{
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Expanded(child:
-            ListView(
-              children: [
-                Container(
-                  height: 20, width: 60
-                  ,child: 
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Text("")
-                    ],
-                  ),
-                ),
-                ListTile(
-                  title: const Text("Meeting"),
-                ),
-                ListTile(
-                  title: const Text("Tasks"),
-                ),
-                ListTile(
-                  title: const Text("Project"),
+            ListView.builder(itemBuilder: (context,index) {
+              return Container(
+                height: 50 , width: 300,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(officeTaskList[index]["task"]),
+                    Spacer(),
+                    ElevatedButton(onPressed: (){
+                      if (officeTaskList[index]["iscompleted"] == false ){
+                        officeTaskList[index]["iscompleted"] = true;
+                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content:
+                        const Text(" ${"task"} is completed")
+                        ,duration: Duration(seconds: 1),
+                        ));
+                      }
+                      else if(officeTaskList[index]["iscompleted"]== true){
+                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content:
+                        const Text(" ${"task"} is  already completed")
+                          ,duration: Duration(seconds: 1),
+                        ));
+                      }
+
+
+                    }, child: const Text("done"))
+
+                  ],
+
                 )
-              ],
+              );
+
+            },
+              itemCount: officeTaskList.length,
             ),
             ),
             ElevatedButton(onPressed: (){
-              Navigator.pop( context ,MaterialPageRoute(builder: (context) => Taskcategories()));
+              Navigator.pop( context ,MaterialPageRoute(builder: (context) => Taskcardscreen()));
             }, child:const Text(" back home"))
           ],
         ),
--- lib/shoppingTaskScreen.dart
+++ lib/shoppingTaskScreen.dart
@@ -8,39 +8,67 @@ class Shoppingtaskscreen extends StatefulWidget{
 
 }
 class _Shoppingtaskscreen extends State<Shoppingtaskscreen>{
+  List<Map<String,dynamic>> shoppinglist = [
+    {"task": "Conditioner" , "isCompleted": false},
+    {"task": "vegies", "isCompleted" : false },
+    {"task": "coldrinks", "isCompleted" : false },
+    {"task": "Mobile charger ", "isCompleted" : false },
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Shopping Tasks"),),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
+        child:
+        Expanded(child: Column(
+
+          mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Expanded(child:
-            ListView(
-              children: [
-                ListTile(
-                  title: const Text("Conditioner"),
-                ),
-                ListTile(
-                  title: const Text("Vegies"),
-                ),
-                ListTile(
-                  title: const Text("cold drinks"),
-                ),
-                ListTile(
-                  title: const Text("Mobile Charger"),
-                )
-              ],
+            ListView.builder(itemBuilder: (context,index){
+              return Container(
+                child:
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(shoppinglist[index]["task"]),
+                    Spacer(),
+                    ElevatedButton(onPressed: (){
+                      if (shoppinglist[index]["isCompleted"]== false){
+                        if(shoppinglist[index]["isCompleted"]== false){
+                          shoppinglist[index]["isCompleted"] = true;
+                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(content:
+                          Text("${"task"} is completed"),
+                            duration: Duration(seconds: 1),));
+                        }
+                        else if(shoppinglist[index]["isCompleted"]== true){
+                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(content:
+                          Text("${"task"} is allready completed"),
+                            duration: Duration(seconds: 1),));
 
-            )),
+                        }
+                      }
+                    }, child:
+                    const Text("done")),
+
+                  ],
+                ),
+              );
+            }
+              ,itemCount: shoppinglist.length,
+            ),
             ElevatedButton(onPressed: (){
               Navigator.pop(context, MaterialPageRoute(builder: (context)=> Taskcategories()));
             }, child: const Text(" go home ") )
-          ],
-
+          ]
+          ,
         ),
+
+          )
+
+        ,
+
       ),
     );
   }
